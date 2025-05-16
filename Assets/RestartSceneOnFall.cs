46608daeba766008fdde6987530c2e7e0cc327e6
BenjaminLeo2008 <166539016+BenjaminLeo2008@users.noreply.github.com>
2025-05-16 09:03:33 -0300

    TP 5 terminado

---
+++ Assets/RestartSceneOnFall.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartSceneOnFall : MonoBehaviour
+{
+    public float deadFallHeight = -15;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < deadFallHeight)
+        {
+            Debug.Log("Moriste XD");
+            SceneManager.LoadScene("FPS Parkour");
+        }
+    }
+}
